317454836ed1487300c318e20ec591f036c32f84
Thomas Male <Tommytron2@yahoo.com.au>
2018-02-21 12:21:25 +1300

    Deleted Unreal Project. Created template for Unity VR. Kept a bow and arrow example from steam VR

--- Prototype_One/Source/Prototype_One/Prototype_One.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Prototype_One.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Prototype_One, "Prototype_One" );
